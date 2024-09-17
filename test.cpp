68ab458410a322964da45f9e708d06651f8edd3f
Afobaboa <gushind1984@gmail.com>
2024-09-17 20:25:19 +0300

    QSort isn't working...

--- test.cpp
+++ test.cpp
@@ -3,6 +3,9 @@
 #include <time.h>
 
 
+//-----------------------------------------------------------------------
+
+
 typedef int (*compareFunc_t) (const void*, const void*);
 
 
@@ -23,82 +26,9 @@ static void Swap(void* firstElemPtr, void* secondElemPtr, const size_t elemSize)
 //------------------------------------------------------------------------------------
 
 
-static const size_t size      = 10;
-static int          arr[size] = {};
-
-
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-
-int partition(int arr[], size_t low, size_t high) {
-
-    // Initialize pivot to be the first element
-    srand(time(NULL));
-    size_t pivotNum = low + (size_t) rand() % (high - low);
-    // printf("Low = %zu, high = %zu, pivotNum = %zu\n", low, high, pivotNum);
-
-    swap(&arr[pivotNum], &arr[low]);
-
-    int p = arr[low];
-    
-    // printf("Pivot = %d\n", p);
-
-    size_t i = low;
-    size_t j = high;
-
-    while (i < j) {
-
-        // Find the first element greater than
-        // the pivot (from starting)
-        while (CompareInt(&arr[i], &p) < 0 && i < high) {
-            i++;
-        }
-
-        // Find the first element smaller than
-        // the pivot (from last)
-        while (CompareInt(&arr[j], &p) >= 0 && j > low) {
-            j--;
-        }
-        if (i < j) {
-            Swap(&arr[i], &arr[j], sizeof(int));
-        }
-    }
-
-    // for (size_t intNum = 0; intNum < size; intNum++)
-    //     printf("%d ", arr[intNum]);
-
-    // printf("\n\n");
-
-    return j;
-}
-
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-
-        // call partition function to find Partition Index
-        int pi = partition(arr, low, high);
-
-        // Recursively call quickSort() for left and right
-        // half based on Partition Index
-        if (pi > low)
-            quickSort(arr, low, pi);
-
-        if (pi < high)
-            quickSort(arr, pi+1, high);
-    }
-}
-
-
-//------------------------------------------------------------------------------
-
-
 static int CompareInt(const void* firstElemPtr, const void* secondElemPtr) 
 {
-    return -(*((int*) firstElemPtr)) + *((int*) secondElemPtr);
+    return -(*((int*) firstElemPtr)) % 10 + *((int*) secondElemPtr) % 10;
 }
 
 
@@ -120,24 +50,30 @@ static size_t Partition(void* array, size_t leftEdge, size_t rightEdge, size_t e
     srand(time(NULL));
     size_t pivotNum = leftEdge + (size_t) rand() % (rightEdge - leftEdge);
 
-    Swap(arr + pivotNum, arr + leftEdge, elemSize);
+    Swap((char*) array + pivotNum, (char*) array + leftEdge, elemSize);
 
-    void* pivotPtr = arr + leftEdge;
+    void* pivotPtr = (char*) array + leftEdge;
 
     size_t leftIterator  = leftEdge;
     size_t rightIterator = rightEdge;
 
     while (leftIterator < rightIterator) 
     {
-        while (Compare(arr + leftIterator, pivotPtr) < 0 && leftIterator < rightEdge) 
+        while (Compare((char*) array + leftIterator, pivotPtr) < 0 && 
+               leftIterator < rightEdge) 
+        {
             leftIterator++;
+        }
         
 
-        while (Compare(arr + rightIterator, pivotPtr) >= 0 && rightIterator > leftEdge) 
+        while (Compare((char*) array + rightIterator, pivotPtr) >= 0 && 
+               rightIterator > leftEdge) 
+        {    
             rightIterator--;
-        
+        }
+
         if (leftIterator < rightIterator) 
-            Swap(arr + leftIterator, arr + rightIterator, elemSize);
+            Swap((char*) array + leftIterator, (char*) array + rightIterator, elemSize);
     }
 
     return rightIterator;
@@ -150,13 +86,13 @@ static void QSort(void* array, size_t leftEdge, size_t rightEdge, size_t elemSiz
     if (leftEdge < rightEdge) 
     {
         // size_t middleEdge = Partition(arr, leftEdge, rightEdge, elemSize, Compare);
-        size_t middleEdge = Partition(arr, leftEdge, rightEdge, elemSize, Compare);
+        size_t middleEdge = Partition(array, leftEdge, rightEdge, elemSize, Compare);
 
         if (middleEdge > leftEdge)
-            QSort(arr, leftEdge, middleEdge, elemSize, Compare);
+            QSort(array, leftEdge, middleEdge, elemSize, Compare);
 
         if (middleEdge < rightEdge)
-            QSort(arr, middleEdge + 1, rightEdge, elemSize, Compare);
+            QSort(array, middleEdge + 1, rightEdge, elemSize, Compare);
     }
 }
 
@@ -164,20 +100,28 @@ static void QSort(void* array, size_t leftEdge, size_t rightEdge, size_t elemSiz
 //---------------------------------------------------------------------------------
 
 
+static void PrintIntArr(int* arr, size_t size)
+{
+for (size_t intNum = 1; intNum < size; intNum++)
+        printf("%d ", arr[intNum]);
+    printf("\n\n");
+}
+
+
 int main() 
 {
+    const size_t size      = 10;
+    int          arr[size] = {};
+
     printf("Size = %zu\n", size);
 
     srand(time(NULL));
     for (size_t intNum = 0; intNum < size; intNum++)
         arr[intNum] = rand() % 400;
-    
+
+    PrintIntArr(arr, size);
     QSort(arr, 0, size - 1, sizeof(int), CompareInt);
-
-    for (size_t intNum = 0; intNum < size; intNum++)
-        printf("%d ", arr[intNum]);
-
-    printf("\n");
+    PrintIntArr(arr, size);
 
     return 0;
 }
