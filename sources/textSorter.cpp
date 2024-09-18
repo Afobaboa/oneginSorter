00846d51bcd5ada5f1556232477c06acbb9916b1
Afobaboa <gushind1984@gmail.com>
2024-09-18 16:56:29 +0300

    Added documentation for static functions of textSorter.cpp

--- sources/textSorter.cpp
+++ sources/textSorter.cpp
@@ -11,46 +11,101 @@
 
 
 /**
+ * Function which compare two elems
+ * using pointers to this elems.
  * 
+ * @param firstElemPtr  Pointer to first elem.
+ * @param secondElemPtr Pointer to second elem.
+ * 
+ * @return Negative int number if first elem
+ *         is bigger than second elem.
+ * @return Zero if first elem is equal to second elem.
+ * @return Positive int number if first elem 
+ *         is bigger than second elem.
  */
-typedef int (*compareFunc_t) (const void*, const void*);
+typedef int (*compareFunc_t) (const void* firstElemPtr, const void* secondELemPtr);
 
 
 /**
+ * This function compare two null-terminated strings.
  * 
+ * This comparator ignores bigger case (change it to lower)
+ * letters and any symbols which aren't letters.
+ * 
+ * @param firstLinePtr  Pointer to first line.
+ * @param secondLinePtr Pointer to second line.
+ * 
+ * @return Negative int number if first line
+ *         is bigger than second line.
+ * @return Zero if first line is equal to second line.
+ * @return Positive int number if first line 
+ *         is bigger than second line.
  */
-static int LineCompare(const void*  firstLine, const void* secondLine);
+static int LineCompare(const void* firstLinePtr, const void* secondLinePtr);
 
 
 /**
+ * This function are moving pointer to 
+ * null-terminated line ahead if there 
+ * isn't a letter. Moving will stop if
+ * there is EOF or '\0' or a letter.
  * 
+ * @param linePointer Pointer to line.
  */
 static void SkipUselessChars(char** linePointer);
 
 
 /**
+ * Quick sorting recursive function.
  * 
+ * @param array     Array of elems.
+ * @param leftEdge  Left edge of sorting area.
+ * @param rightEdge Right edge of sorting area.
+ * @param elemSize  Size of elems in bytes.
+ * @param Compare   Function which compares elems.
  */
 static void QSort(void* array, size_t leftEdge, size_t rightEdge, size_t elemSize, 
                   compareFunc_t Compare);
 
 
 /**
+ * Function which check if elems
+ * of array are sorted in right order.
  * 
+ * @param array     Array of sorting elements.
+ * @param elemCount Count of sorted elems.
+ * @param elemSize  Size of sorting elems in bytes.
+ * @param Compare   Function which compares elems.
+ * 
+ * @return true if elems are in right order,
+ * @return false in other situations.
  */
-static bool QSortTest(void* array, size_t elemCount, size_t elemSize, 
+static bool SortTest(void* array, size_t elemCount, size_t elemSize, 
                       compareFunc_t Compare);
 
 
 /**
+ * This function do partition for QSort algorythm.
  * 
+ * @param array     Pointer to array of data.
+ * @param leftEdge  Left edge of sorting area.
+ * @param rightEdge Right edge of sorting area.
+ * @param elemSize  Size of sorting elems in bytes.
+ * @param Compare   Function for comparing elems.
+ * 
+ * @return Count of elems wich less than random pivot.
  */
 static size_t Partition(void* array, size_t leftEdge, size_t rightEdge, size_t elemSize, 
                         compareFunc_t Compare);
 
 
 /**
+ * This function swapping memory
+ * of elements by their pointers.
  * 
+ * @param firstElemPtr  Pointer to first elem.
+ * @param secondElemPtr Pointer to second elem.
+ * @param elemSize      Size of elems in bytes.
  */
 static void Swap(void* firstElemPtr, void* secondElemPtr, const size_t elemSize);
 
@@ -58,7 +113,7 @@ static void Swap(void* firstElemPtr, void* secondElemPtr, const size_t elemSize)
 void SortTextLines(Text* text) 
 {
     QSort(text->linePointers, 0, text->lineCount - 1, sizeof(char*), LineCompare);
-    QSortTest(text->linePointers, text->lineCount, sizeof(char*), LineCompare);
+    SortTest(text->linePointers, text->lineCount, sizeof(char*), LineCompare);
 }
 
 
@@ -167,8 +222,8 @@ static void QSort(void* array, size_t leftEdge, size_t rightEdge, size_t elemSiz
 }
 
 
-static bool QSortTest(void* array, size_t elemCount, size_t elemSize, 
-                      compareFunc_t Compare)
+static bool SortTest(void* array, size_t elemCount, size_t elemSize, 
+                     compareFunc_t Compare)
 {
     for (size_t elemNum = 1; elemNum < elemCount; elemNum++)
     {
